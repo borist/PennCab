c722452602c0a2b928be20cef1ed2abef5d7133e
Boris Treskunov <borist@seas.upenn.edu>
2013-04-23 22:26:58 -0400

    removed postgresql credentials

--- PennCab/PennCab/settings.py
+++ PennCab/PennCab/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'penncab',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'borist',
-        'PASSWORD': '1213',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '', #                      # Set to empty string for default.
     }
